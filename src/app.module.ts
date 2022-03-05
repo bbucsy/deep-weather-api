615c38027866127b5a2991c6a16c7913c6910704
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-03-05 21:25:47 +0100

    Rearrange src folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { UserModule } from './user/user.module';
+import { UserModule } from './app/user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UserModule],
