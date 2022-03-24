3627e0f244a95a74d557a5d69f9f989f3c63c320
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-03-24 15:54:34 +0100

    refactor prediction module

--- src/app/prediction/prediction.module.ts
+++ src/app/prediction/prediction.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NeuralModelModule } from '../neural-model/neural-model.module';
-import { OpenWeatherModule } from '../open-weather/open-weather.module';
 import { Prediction } from './prediction.entity';
 import { PredictionService } from './prediction.service';
 
 @Module({
-  imports: [
-    OpenWeatherModule,
-    NeuralModelModule,
-    TypeOrmModule.forFeature([Prediction]),
-  ],
+  imports: [TypeOrmModule.forFeature([Prediction])],
   providers: [PredictionService],
   exports: [PredictionService],
 })
--- src/app/prediction/prediction.service.ts
+++ src/app/prediction/prediction.service.ts
@@ -1,40 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { NeuralModelService } from '../neural-model/neural-model.service';
-import { Predictor } from '../neural-model/predictor';
-import { OpenWeatherService } from '../open-weather/open-weather.service';
 import { Prediction } from './prediction.entity';
+import { CreatePredictionDto } from './dto/create-prediction.dto';
 
 @Injectable()
 export class PredictionService {
   constructor(
     @InjectRepository(Prediction)
     private readonly predictionRepository: Repository<Prediction>,
-    private readonly modelService: NeuralModelService,
-    private readonly weatherService: OpenWeatherService,
   ) {}
-
-  async predictWeather(model_id: number): Promise<Prediction> {
-    const model = await this.modelService.findOne(model_id, true);
-    const predictor = await model.getPredictor();
-    const data = await this.weatherService.getHistoricalHorlyData(
-      model.city.lat.toString(),
-      model.city.lon.toString(),
-      Predictor.LAG,
-    );
-    const result = await predictor.predict(data);
-    const prediction = this.predictionRepository.create({
-      input: JSON.stringify(data),
-      model: model,
-      result: result,
-    });
-    this.predictionRepository.save(prediction);
-    return prediction;
-  }
 
   async findAllPredictions(): Promise<Prediction[]> {
     return this.predictionRepository.find();
+  }
+
+  async create(createDto: CreatePredictionDto): Promise<Prediction> {
+    const prediction = this.predictionRepository.create({
+      input: JSON.stringify(createDto.input),
+      model: createDto.model,
+      result: createDto.result,
+    });
+    return this.predictionRepository.save(prediction);
   }
 
   async findByCity(city_id: number): Promise<Prediction[]> {
