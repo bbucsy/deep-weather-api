330a559d08cb7314d50d5b9d8cba92e675f1a44c
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-03-30 01:17:45 +0200

    remove frontpage response listing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,12 @@
 import { Controller, Get, Render } from '@nestjs/common';
-import { PredictionService } from './app/prediction/prediction.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly predictionService: PredictionService) {}
-
   @Get()
   @Render('index')
   async getHello() {
     return {
-      //message: await this.weatherApi.currentWeather('47.4979', '19.0402'),
-      message: await this.predictionService.findByCity(1),
+      message: 'Welcome',
     };
   }
 }
