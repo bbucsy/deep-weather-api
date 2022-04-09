b13f2f93a989d0c2029ac178660417da547c4e59
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-04-09 21:35:52 +0200

    Configude city module swagger

--- src/app/city/city.controller.ts
+++ src/app/city/city.controller.ts
@@ -10,11 +10,18 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { City } from './city.entity';
 import { CityService } from './city.service';
 import { CityDto } from './dto/city.dto';
 import { CreateCityDto } from './dto/create-city.dto';
 
+@ApiTags('city')
 @Controller('city')
 export class CityController {
   constructor(private readonly cityService: CityService) {}
@@ -30,11 +37,14 @@ export class CityController {
   }
 
   @Get()
+  @ApiOkResponse({ type: [CityDto] })
   async findAll(): Promise<CityDto[]> {
     return (await this.cityService.findAll()).map(this.cityToDto);
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: CityDto })
+  @ApiNotFoundResponse({ description: 'City not found with specified ID' })
   async findOne(@Param('id') id: number): Promise<CityDto> {
     const city = await this.cityService.findOne(id);
     if (!city) throw new NotFoundException();
@@ -42,6 +52,8 @@ export class CityController {
   }
 
   @Delete(':id')
+  @ApiOkResponse({ description: 'City with specified ID is deleted' })
+  @ApiResponse({ status: 204, description: 'No city found with this id' })
   async remove(@Param('id') id: string) {
     const removed = await this.cityService.remove(+id);
     this.logger.debug(removed);
--- src/app/city/dto/city.dto.ts
+++ src/app/city/dto/city.dto.ts
@@ -1,7 +1,31 @@
-export interface CityDto {
+import { ApiProperty } from '@nestjs/swagger';
+
+class neuralModelBase {
   id: number;
   name: string;
+}
+
+export class CityDto {
+  /**
+   * ID of the city
+   */
+  id: number;
+  /**
+   * Name of the city
+   */
+  name: string;
+  /**
+   * Latitude
+   */
   lat: number;
+  /**
+   * Longitude
+   */
   lon: number;
+
+  /**
+   * Connected Neural models
+   */
+  @ApiProperty({ type: [neuralModelBase] })
   neuralModels: { id: number; name: string }[];
 }
